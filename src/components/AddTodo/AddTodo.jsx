ca4e9bb10772bc57347cd3b68f7e1fe7f18d3983
Kiryl <bobkovum@gmail.com>
2022-12-11 23:53:44 +0300

    feat: made components

---
+++ src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddTodo() {
+  return <div>Form</div>;
+}
+
+export default AddTodo;
---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function TodoList({ todo, setTodo }) {
+  function deleteTodo(id) {
+    let newTodo = [...todo].filter((item) => item.id !== id);
+    setTodo(newTodo);
+  }
+
+  return (
+    <div>
+      {todo.map((item) => (
+        <div key={item.id}>
+          <div>{item.title}</div>
+          <button onClick={() => deleteTodo(item.id)}>Del</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TodoList;
