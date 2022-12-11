1dc4b8fb6ee8f96a8063845f13cb376b420924bc
Kiryl <bobkovum@gmail.com>
2022-12-12 01:19:39 +0300

    feat: save todo

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import uuid from "uuid";
 
-function AddTodo() {
-  return <div>Form</div>;
+function AddTodo({ todo, setTodo }) {
+  const [value, setValue] = useState("");
+
+  console.log(value);
+
+  function saveTodo() {
+    setTodo([
+      ...todo,
+      {
+        id: uuid.v4(),
+        title: value,
+      },
+    ]);
+  }
+  return (
+    <div>
+      <input
+        placeholder="Add note"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={saveTodo}>Save</button>
+    </div>
+  );
 }
 
 export default AddTodo;
